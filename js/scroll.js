d15908d4487bff5d2e8f8bae8267ab853ccf4fdc
Alyona <ogavelaoi@gmail.com>
2019-07-21 09:35:27 +0300

    up button added some bugs fixed

---
+++ js/scroll.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.up').click(function(){
+       $('html, body').animate({scrollTop:$('#up').position().top - 60}, 2000);
+    });
+
+    $(window).scroll(function(){
+      if($(window).scrollTop()>200){
+      $('.up').show()
+    } else {
+      $('.up').hide()
+    }
+    });
+});
